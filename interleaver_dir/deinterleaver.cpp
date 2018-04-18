81d056ae95e1e744c11714afee6f66faf479530b
Lenoy Avidan <lenoyavidan@Lenoys-MacBook-Pro.local>
2018-04-18 11:23:25 -0700

    deinterleaver tweaked forecast

--- interleaver_dir/deinterleaver.cpp
+++ interleaver_dir/deinterleaver.cpp
@@ -4,14 +4,14 @@ using namespace gr;
 class deinterleaver : public gr_block {
 public:
 	deinterleaver_impl::deinterleaver_impl()
-		: gr::block("interleaver", gr::io_signature::make(2, 2, sizeof (int)), gr::io_signature::make(1, 1, sizeof (int) * 2)) 
+		: gr::block("interleaver", gr::io_signature::make(1, 1, sizeof (int)), gr::io_signature::make(2, 2, sizeof (int) * 2)) 
 	{
 
 	}
 
 	void deinterleaver_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
 	{
-		ninput_items_required[0] = noutput_items/2;
+		ninput_items_required[0] = noutput_items*2;
 	}
 
 	int deinterleaver_impl::general_work(int noutput_items,
