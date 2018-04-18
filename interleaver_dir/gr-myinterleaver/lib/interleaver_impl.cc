6d337871ad021880c4ddfe10fd2550b33fcfed89
Lenoy Avidan <lenoyavidan@Lenoys-MacBook-Pro.local>
2018-04-18 12:03:26 -0700

    implemented interleaver code

--- interleaver_dir/gr-myinterleaver/lib/interleaver_impl.cc
+++ interleaver_dir/gr-myinterleaver/lib/interleaver_impl.cc
@@ -40,8 +40,8 @@ namespace gr {
      */
     interleaver_impl::interleaver_impl()
       : gr::block("interleaver",
-              gr::io_signature::make(<+MIN_IN+>, <+MAX_IN+>, sizeof(<+ITYPE+>)),
-              gr::io_signature::make(<+MIN_OUT+>, <+MAX_OUT+>, sizeof(<+OTYPE+>)))
+              gr::io_signature::make(2, 2, sizeof(int)),
+              gr::io_signature::make(1, 1, sizeof(int)))
     {}
 
     /*
@@ -55,6 +55,7 @@ namespace gr {
     interleaver_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
     {
       /* <+forecast+> e.g. ninput_items_required[0] = noutput_items */
+      ninput_items_required[0] = noutput_items/2;
     }
 
     int
@@ -63,13 +64,21 @@ namespace gr {
                        gr_vector_const_void_star &input_items,
                        gr_vector_void_star &output_items)
     {
-      const <+ITYPE+> *in = (const <+ITYPE+> *) input_items[0];
-      <+OTYPE+> *out = (<+OTYPE+> *) output_items[0];
+      const int *in1 = (const int *) input_items[0];
+      const int *in2 = (const int *) input_items[1];
+      const int *out = (const int *) output_items[0];
+
+      for (int i=0; i < noutput_items, i += 2) {
+        out[i] = in1[i/2]
+        if (i+1 < noutput_items) {
+          out[i+1] = in2[i/2]
+        }
+      }
 
       // Do <+signal processing+>
       // Tell runtime system how many input items we consumed on
       // each input stream.
-      consume_each (noutput_items);
+      consume_each (noutput_items/2);
 
       // Tell runtime system how many output items we produced.
       return noutput_items;
